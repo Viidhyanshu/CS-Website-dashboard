49f3e0d7e9ab6460eb5686f0121b1d9ee51a3fc5
Tanmoy mandal <tanmaymandal1250@gmail.com>
2026-06-18 15:39:15 +0530

    Fix postgres.js socket config - use standard options

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -2,11 +2,8 @@ import postgres from 'postgres';
 
 const sql = postgres(process.env.DATABASE_URL!, {
   prepare: false,
-  idle_timeout: 20, // Close idle connections after 20 seconds
-  max_lifetime: 60 * 60, // Max connection lifetime 1 hour
-  socket: {
-    keepalive: true,
-  }
+  idle_timeout: 20,
+  max_lifetime: 60 * 60,
 });
 
 export default sql;
